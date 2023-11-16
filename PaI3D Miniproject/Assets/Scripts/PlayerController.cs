9f06819c0efe93e4c1a3e3430bd00d2d2fa7fb93
TzeHuoGucciHo <tho22@student.aau.dk>
2023-11-16 15:09:27 +0100

    Scene set up, first person movement script (with Lerp), no jump

---
+++ PaI3D Miniproject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Transform playerCamera;
+
+    private Vector2 movementInput;
+    private Vector3 currentVelocity;
+    public float movementSpeed = 10f;
+    public float acceleration = 2f;
+    public float deceleration = 3f;
+
+    private Vector2 mouseInput;
+    public float mouseSensitivity = 1f;
+    public float minCameraAngle = -90f;
+    public float maxCameraAngle = 90f;
+    private void Start()
+    {
+                                                                                                                            //Gets the components from the player object and assigns them to the corresponding variables.
+        rb = GetComponent<Rigidbody>();
+        playerCamera = GetComponentInChildren<Camera>().transform;
+    }
+
+    private void Update()
+    {
+                                                                                                                            //Locks the cursor to the center of the screen and makes it invisible.
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+                                                                                                                            //Gets the input from the player using the Unity Input System and assigns them to the corresponding variables.
+                                                                                                                            //Normalized makes sure that the player moves at the same speed in all directions.
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        OnMove();
+        OnLook();
+    }
+
+    private void OnMove()
+    {
+                                                                                                                            //Gets the forward direction of the camera and sets the y-value to 0 to make sure the player moves in the direction the camera is facing.
+        Vector3 cameraForward = playerCamera.forward;
+        cameraForward.y = 0f;
+        cameraForward.Normalize();
+                                                                                                                            //Calculates the target velocity and uses Lerp to make the player accelerate and decelerate smoothly towards the target velocity.
+        Vector3 targetVelocity = (cameraForward * movementInput.y + playerCamera.right * movementInput.x) * movementSpeed;
+                                                                                                                            //Uses Lerp to make the player accelerate and decelerate smoothly towards the target velocity.
+        currentVelocity = Vector3.Lerp(currentVelocity, targetVelocity, Time.deltaTime * acceleration);
+                                                                                                                            //If the player's movement input is less than 0.1, the player will start to decelerate, lerping the current velocity towards zero.
+        if (movementInput.magnitude < 0.1f)
+        {
+            currentVelocity = Vector3.Lerp(currentVelocity, Vector3.zero, Time.deltaTime * deceleration);
+        }
+                                                                                                                            //Sets the velocity of the player to the current velocity.
+        rb.velocity = currentVelocity;
+    }
+    private void OnLook()
+    {
+                                                                                                                            //Calculates horizontal rotation based on the mouse input along the x-axis.
+        float rotationY = transform.localEulerAngles.y + mouseInput.x * mouseSensitivity;
+                                                                                                                            //Applies the horizontal rotation to the player object's local rotation.
+        transform.localRotation = Quaternion.Euler(0f, rotationY, 0f);
+                                                                                                                            //Calculates the vertical rotation based on the mouse input along the y-axis.
+        float rotationX = playerCamera.localEulerAngles.x - mouseInput.y * mouseSensitivity; 
+                                                                                                                            //If the vertical rotation is greater than 180, it will be subtracted by 360 to make sure the rotation is always between -180 and 180.
+                                                                                                                            //This makes sure that the player can you look up and down without the camera flipping.
+                if (rotationX > 180f)
+                {
+                    rotationX -= 360f;
+                }
+                                                                                                                            //Clamps the vertical rotation between the min and max camera angle.
+        rotationX = Mathf.Clamp(rotationX, minCameraAngle, maxCameraAngle);
+                                                                                                                            //Applies the vertical rotation to the camera object's local rotation.
+        playerCamera.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+    }
+}
